526e698f23fd84428b3f65afb465c09fac4c744c
Jay S <jpseo84@gmail.com>
2023-03-06 14:14:43 +0900

    Added comments and modified output format

--- check_balance.py
+++ check_balance.py
@@ -3,16 +3,19 @@ import requests
 import time
 
 def check_balance(currency, address):
+    #BTC balance request using API from Blockchain.info, free
     if currency == 'BTC':
         api_url = f"https://blockchain.info/rawaddr/{address}"
         response = requests.get(api_url).json()
         return response['final_balance'] / 10**8
-
+    
+    #Ethereum balance request using API from etherscan.io, free tier
     elif currency == 'ETH':
         api_url = f"https://api.etherscan.io/api?module=account&action=balance&address={address}&tag=latest&apikey=YourApiKeyToken"
         response = requests.get(api_url).json()
         return int(response['result']) / 10**18
 
+    #Ripple balance request using API from Ripple, free
     elif currency == 'XRP':
         api_url = f"https://data.ripple.com/v2/accounts/{address}/balances"
         response = requests.get(api_url).json()
@@ -21,11 +24,13 @@ def check_balance(currency, address):
                 return float(balance['value'])
         return None
 
+    #Litecoin(LTC) balance request using Blockcypher's API, free tier
     elif currency == 'LTC':
         api_url = f"https://api.blockcypher.com/v1/ltc/main/addrs/{address}/balance"
         response = requests.get(api_url).json()
         return response['balance'] / 10**8
     
+    #Dogecoin(DOGE) balance request using Blockcypher's API, free tier
     elif currency == 'DOGE':
         api_url = f"https://api.blockcypher.com/v1/doge/main/addrs/{address}/balance"
         response = requests.get(api_url).json()
@@ -41,7 +46,7 @@ with open('wallet.csv', 'r') as csvfile:
         address = row[1]
         balance = check_balance(currency, address)
         if balance is not None:
-            print(f"{currency} - {address}: {balance}")
+            print(f"{currency},{address},{balance}")
         else:
-            print(f"{currency} - {address}: Error retrieving data")
+            print(f"{currency},{address},'Error retrieving data'")
         time.sleep(11)
